3067c1fe858c589409038fb851f9ccdcf82c6e7b
radswiat <rad.swiat@gmail.com>
2017-08-03 13:12:35 +0100

    Add more comments

--- src/js/core/utils/utils.js
+++ src/js/core/utils/utils.js
@@ -1,3 +1,18 @@
+/**
+ * Is defined
+ * makes code looks better without typeof everywhere
+ * @param val
+ * @return {boolean}
+ */
+export function isDefined(val) {
+	return typeof val !== 'undefined';
+}
+
+/**
+ * Clear all html child nodes
+ * @param node
+ * @return {boolean}
+ */
 export function clearAllNodes(node) {
 	if (!node) {
 		return false;
@@ -8,12 +23,22 @@ export function clearAllNodes(node) {
 	return true;
 }
 
+/**
+ * Convert string into html node
+ * @param string
+ * @return {Node}
+ */
 export function stringToHtmlNode(string) {
 	let div = document.createElement('div');
 	div.innerHTML = string;
 	return div.firstChild.cloneNode(true);
 }
 
+/**
+ * Custom defer,
+ * return promise and resolver/reject
+ * @return {deferred promise}
+ */
 export function Defer() {
 	this.promise = new Promise((resolve, reject) => {
 		this.resolve = resolve;
@@ -23,14 +48,23 @@ export function Defer() {
 	this.promise.catch = this.promise.catch.bind(this.promise);
 }
 
-export function isDefined(val) {
-	return typeof val !== 'undefined';
-}
-
+/**
+ * Get uniform random number
+ * @param max
+ * @param min
+ * @return {int}
+ */
 export function getUniformRandomNumber(max, min) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+/**
+ * Get falling random number
+ * - beginning will be prioritized
+ * @param max
+ * @param mod - priority modified
+ * @return {int}
+ */
 export function getFallingRandomNumber(max, mod) {
 	let rndNumber = null;
 	for (let i = 0; i < max; i++) {
